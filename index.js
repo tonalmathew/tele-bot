d360f39dcefd89e8f0fee357843b11fa793c9c4a
tonalmathew <tonalmathew1999@gmail.com>
2020-10-31 15:06:10 +0530

    updated with telegram token

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ const intro = require("./api/intro");
 dotenv.config();
 
 const bot = new TG({
-  token: process.env.TELE_API_KEY,
+    token: process.env.BOT_TOKEN,
 });
 
 const mp = new TG.GetUpdateMessageProvider();
 
 bot.setMessageProvider(mp);
 bot
-  .start()
-  .then(() => {
-    console.log("API is started");
-  })
-  .catch(console.err);
+    .start()
+    .then(() => {
+        console.log("API is started");
+    })
+    .catch(console.err);
 
 bot.on("update", (update) => {
-  const [message, chat_id, name] = [
-    update.message.text,
-    update.message.chat.id,
-    update.message.chat.first_name
-  ];
+    const [message, chat_id, name] = [
+        update.message.text,
+        update.message.chat.id,
+        update.message.chat.first_name
+    ];
 
-  if (message.startsWith("/")) {
-    switch (message) {
-      case "/start": intro.start(name, bot, chat_id); break;
+    if (message.startsWith("/")) {
+        switch (message) {
+            case "/start":
+                intro.start(name, bot, chat_id);
+                break;
+        }
     }
-  }
-  getDetails(message, chat_id, bot);
+    getDetails(message, chat_id, bot);
 });
