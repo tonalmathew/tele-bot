fa0129e089f46e5dac4f315a4b08a91684ce1a3f
tonalmathew <tonalmathew1999@gmail.com>
2020-10-31 10:41:06 +0530

    Procfile

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tele-bot
+ 
--- index.js
+++ index.js
@@ -5,24 +5,27 @@ const getDetails = require("./api/getDetails.api.github");
 dotenv.config();
 
 const api = new TG({
-  token: process.env.TELE_API_KEY,
+    token: process.env.BOT_TOKEN,
 });
 
 const mp = new TG.GetUpdateMessageProvider();
 
 api.setMessageProvider(mp);
 api
-  .start()
-  .then(() => {
-    console.log("API is started");
-  })
-  .catch(console.err);
+    .start()
+    .then(() => {
+        console.log("API is started");
+    })
+    .catch(console.err);
 
 api.on("update", (update) => {
-  const [messageWithUserName, chat_id] = [
-    update.message.text,
-    update.message.chat.id,
-  ];
+    const [messageWithUserName, chat_id] = [
+        update.message.text,
+        update.message.chat.id,
+    ];
 
-  getDetails(messageWithUserName, chat_id, api);
+    getDetails(messageWithUserName, chat_id, api);
 });
+
+// mysterious-earth-56254
+// https://mysterious-earth-56254.herokuapp.com/
