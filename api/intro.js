9f5843a353a31ab602a6ea2633e004cf355b401a
tonalmathew <tonalmathew1999@gmail.com>
2020-10-31 14:50:27 +0530

    hide heroku login details

--- api/intro.js
+++ api/intro.js
@@ -2,7 +2,7 @@ module.exports = {
     start: (name, bot, chat_id) => {
         bot.sendMessage({
             chat_id: chat_id,
-            text: `Hey, ${name} i am \*Amminathatha* you can call me amina😅`,
+            text: `Hey ${name}, I am \*Amminathatha* you can call me amina😅`,
             parse_mode: "Markdown"
         })
 
