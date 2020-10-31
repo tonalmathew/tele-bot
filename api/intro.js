00bfbafb64f3514a7e33067676f8badab0f2dd24
tonalmathew <tonalmathew1999@gmail.com>
2020-10-31 15:22:31 +0530

    [bug]: :bug: bug fixed

--- api/intro.js
+++ api/intro.js
@@ -2,7 +2,7 @@ module.exports = {
     start: (name, bot, chat_id) => {
         bot.sendMessage({
             chat_id: chat_id,
-            text: `Hey ${name}, I am \*Amminathatha* you can call me amina😅`,
+            text: `Hey ${name}, I am \*Aaminathatha* you can call me Aamina😅`,
             parse_mode: "Markdown"
         })
 
