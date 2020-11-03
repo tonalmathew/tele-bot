8c841efe4b0db42d38984522e895fe9475c3b57d
tonalmathew <tonalmathew1999@gmail.com>
2020-11-03 10:40:44 +0530

    [start]: :bug: fixed bug

--- index.js
+++ index.js
@@ -3,38 +3,39 @@ const dotenv = require("dotenv");
 const getDetails = require("./api/getDetails.api.github");
 const intro = require("./api/commands");
 const greetings = require("./api/greetings");
+const commands = require("./api/commands");
 
 dotenv.config();
 
 const bot = new TG({
-  token: process.env.BOT_TOKEN,
+    token: process.env.BOT_TOKEN,
 });
 
 const mp = new TG.GetUpdateMessageProvider();
 
 bot.setMessageProvider(mp);
 bot
-  .start()
-  .then(() => {
-    console.log("API is started");
-  })
-  .catch(console.err);
+    .start()
+    .then(() => {
+        console.log("API is started");
+    })
+    .catch(console.err);
 
 bot.on("update", (update) => {
-  // console.log(update);
-  const [message, chat_id, name] = [
-    update.message.text,
-    update.message.chat.id,
-    update.message.from.first_name,
-  ];
+    // console.log(update);
+    const [message, chat_id, name] = [
+        update.message.text,
+        update.message.chat.id,
+        update.message.from.first_name,
+    ];
 
-  if (message.startsWith("/")) {
-    switch (message) {
-      case "/start":
-        intro.start(name, bot, chat_id);
-        break;
+    if (message.startsWith("/")) {
+        switch (message) {
+            case "/start":
+                commands(name, bot, chat_id);
+                break;
+        }
     }
-  }
-  greetings(message, chat_id, name, bot);
-  getDetails(message, chat_id, bot);
+    greetings(message, chat_id, name, bot);
+    getDetails(message, chat_id, bot);
 });
