875d0f744e9cf40bf7ebf7432c4a39e4835092d6
shan shaji <shanshaji224@gmail.com>
2020-11-03 09:59:27 +0530

    feat: :sparkles: added greetings file and reformated code

---
+++ api/greetings.js
@@ -0,0 +1,27 @@
+module.exports = (greetingMessage, chat_id, name, bot) => {
+  greetingMessage = greetingMessage.toString().toLowerCase();
+
+  const [bye, goodNight, goodMorning] = ["bye", "good night", "good morning"];
+
+  if (greetingMessage.includes(bye)) {
+    bot.sendMessage({
+      chat_id: chat_id,
+      text:
+        "Hope to see you around again Bye!! \nപുറത്തുപോകുക \nഉടൻ കാണാം " +
+        name,
+      parse_mode: "Markdown",
+    });
+  } else if (greetingMessage.includes(goodMorning)) {
+    bot.sendMessage({
+      chat_id: chat_id,
+      text: "Good Morning " + name + "😇",
+      parse_mode: "Markdown",
+    });
+  } else if (greetingMessage.includes(goodNight)) {
+    bot.sendMessage({
+      chat_id: chat_id,
+      text: "Good Night " + name + "😴",
+      parse_mode: "Markdown",
+    });
+  }
+};
